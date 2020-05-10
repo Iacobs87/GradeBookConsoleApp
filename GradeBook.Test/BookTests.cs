c4b769987279b83d5b3c0918ca6df8fbd65e06d5
Alexandru Iacoboae <alexiacoboae@gmail.com>
2020-05-10 19:18:35 +0300

    Adding the Unit Test Project

---
+++ GradeBook.Test/BookTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GradeBook.Test
+{
+    [TestClass]
+    public class BookTests
+    {
+        [TestMethod]
+        [TestCategory("Unit Test")]
+        public void TestMethod1()
+        {
+            //arange 
+
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            //act
+            var result = book.GetStatistics();
+
+            //assert
+            Assert.AreEqual(85.6, result.Average, 1);
+            Assert.AreEqual(90.5, result.High, 1);
+            Assert.AreEqual(77.3, result.Low, 1);
+
+        }
+    }
+}
---
+++ GradeBook.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradeBook.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradeBook.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d64c16e-529d-4c83-9626-d70456040e48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
